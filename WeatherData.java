24828a880309477e85d8c7b33c8b8483297ffd13
kazuma_48 <203885136wm@gmail.com>
2025-05-29 09:24:31 +0900

    一部修正

--- WeatherData.java
+++ WeatherData.java
@@ -2,21 +2,21 @@ public class WeatherData {
     private final String name;
     private final double latitude;
     private final double longitude;
- 
+
     public WeatherData(String name, double latitude, double longitude) {
         this.name = name;
         this.latitude = latitude;
         this.longitude = longitude;
     }
- 
+
     public String getName() {
         return name;
     }
- 
+
     public double getLatitude() {
         return latitude;
     }
- 
+
     public double getLongitude() {
         return longitude;
     }
