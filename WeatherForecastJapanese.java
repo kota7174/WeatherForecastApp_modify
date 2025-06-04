5f296d228577c934026c45d46096759968828a4d
kazuma_48 <203885136wm@gmail.com>
2025-06-04 11:24:21 +0900

    変更

--- WeatherForecastJapanese.java
+++ WeatherForecastJapanese.java
@@ -5,7 +5,6 @@ import javax.swing.JEditorPane;
 import javax.swing.JScrollPane;
 
 public class WeatherForecastJapanese {
-
     public static void main(String[] args) throws IOException, InterruptedException {
         List<WeatherData> datas = List.of(
                 new WeatherData("岐阜", 35.4233, 136.7606),
@@ -14,16 +13,17 @@ public class WeatherForecastJapanese {
                 new WeatherData("三重", 34.7303, 136.5086));
 
         WeatherApiClient client = new WeatherApiClient();
-
         String html = WeatherResultPrinter.getWeatherForecastsHtml(datas, client)
                 + WeatherMethodlist.getWeatherUranaiHtml();
 
         javax.swing.SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("天気予報＆占い（HTML表示）");
-            JEditorPane editorPane = new JEditorPane("text/html",
-                    "<html><body style='font-family:sans-serif; background:#f7fafd; margin:0; padding:24px;'>" + html
-                            + "</body></html>");
+            JFrame frame = new JFrame("天気予報");
+            JEditorPane editorPane = new JEditorPane();
+            editorPane.setContentType("text/html");
             editorPane.setEditable(false);
+            editorPane
+                    .setText("<html><body style='font-family:sans-serif; background:#f7fafd; margin:0; padding:24px;'>"
+                            + html + "</body></html>");
             frame.getContentPane().add(new JScrollPane(editorPane));
             frame.setSize(700, 800);
             frame.setLocationRelativeTo(null);
